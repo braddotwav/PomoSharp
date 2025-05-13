6bce39bc53e35b8928c57a22ee7c8985cc8f3aa4
Brad <90891134+braddotwav@users.noreply.github.com>
2025-05-13 22:31:56 +0100

    Update NavigationService.cs

--- PomoSharp/Services/NavigationService.cs
+++ PomoSharp/Services/NavigationService.cs
@@ -8,17 +8,19 @@ public class NavigationService : INavigationService
 
     public event Action<ViewModelBase>? OnNavigationChanged;
 
+    private ViewModelBase? _currentViewmodel;
+
     public void Change<TViewModel>() where TViewModel : ViewModelBase
     {
         ViewModelBase? viewModel = _registeredViewModels.FirstOrDefault(x => x.GetType() == typeof(TViewModel));
 
-        if (viewModel is not null)
-        {
-            OnNavigationChanged?.Invoke(viewModel);
-            return;
-        }
-
-        throw new InvalidOperationException($"ViewModel of type {typeof(TViewModel)} has not been registered.");
+        if (viewModel is null)
+            throw new InvalidOperationException($"ViewModel of type {typeof(TViewModel)} has not been registered.");
+        
+        _currentViewmodel?.OnViewHide();
+        _currentViewmodel = viewModel;
+        _currentViewmodel?.OnViewShow();
+        OnNavigationChanged?.Invoke(viewModel);
     }
 
     public void Register(ViewModelBase viewModel)
