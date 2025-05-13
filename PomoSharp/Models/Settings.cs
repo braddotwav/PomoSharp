360288362d99a0c08dd7e7dfe3f45ab7044a0002
Brad <90891134+braddotwav@users.noreply.github.com>
2025-05-13 01:14:30 +0100

    Remove theme option

--- PomoSharp/Models/Settings.cs
+++ PomoSharp/Models/Settings.cs
@@ -9,13 +9,8 @@ public class Settings : IEquatable<Settings>
     public bool ShouldAutoStartBreak { get; set; } = false;
     public bool ShouldAutoStartPomodoro { get; set; } = false;
     public bool AllowNotifications { get; set; } = true;
-    public Theme Theme { get; set; } = Theme.LIGHT;
     public WindowBounds? WindowBounds { get; set; }
 
-public enum Theme
-{
-    LIGHT = 0,
-    DARK = 1,
     public Settings() { }
 
     public Settings(Settings settings)
