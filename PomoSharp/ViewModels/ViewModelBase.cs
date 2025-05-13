ee259b1f3d4896322a460b9576f379956c4ae3d1
Brad <90891134+braddotwav@users.noreply.github.com>
2025-05-13 01:18:15 +0100

    Add Show and Hide methods to viewmodels

--- PomoSharp/ViewModels/ViewModelBase.cs
+++ PomoSharp/ViewModels/ViewModelBase.cs
@@ -5,4 +5,7 @@ namespace PomoSharp.ViewModels;
 public abstract class ViewModelBase : ObservableObject
 {
     public abstract string Name { get; }
+
+    public abstract void OnViewShow();
+    public abstract void OnViewHide();
 }
